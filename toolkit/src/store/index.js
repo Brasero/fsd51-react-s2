6d6cf22eda10dd644b54d2afdc4b063982d09613
Brasero <brandonricci4@gmail.com>
2024-04-24 14:39:41 +0200

    middleware presentation

--- toolkit/src/store/index.js
+++ toolkit/src/store/index.js
@@ -1,14 +1,26 @@
 import {configureStore} from "@reduxjs/toolkit";
 import messageSlice from "./slice/messageSlice.js";
-import countSlice from "./slice/countSlice.js";
+import countSlice, {increment} from "./slice/countSlice.js";
 import taskSlice from "./slice/taskSlice.js";
+
+const middleware = (store) => (next) => (action) => {
+  console.log(action)
+  if (action.type !== increment.toString()) {
+    store.dispatch(increment())
+  }
+  next(action)
+}
+
 
 const store = configureStore({
   reducer: {
     message: messageSlice,
     count: countSlice,
     tasks: taskSlice
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([
+    middleware
+  ])
 })
 
 export default store
